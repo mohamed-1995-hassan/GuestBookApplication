871c7a71ab662466b6d02469d98e27fb24c03575
mohamed-1995-hassan <59643006+mohamed-1995-hassan@users.noreply.github.com>
2021-12-08 00:02:19 +0200

    Update_Fields

--- GuestBookApplication.Data/Models/User.cs
+++ GuestBookApplication.Data/Models/User.cs
@@ -12,6 +12,7 @@ namespace GuestBookApplication.Data.Models
         [Required]
         public string UserName { get; set; }
         [Required]
+        [EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
